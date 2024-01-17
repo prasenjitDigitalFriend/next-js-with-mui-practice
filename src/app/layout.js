2bd7dd09956ee4e0ccc346ad4da63bd6ee4657a0
prasenjitDigitalFriend <100418932+prasenjitDigitalFriend@users.noreply.github.com>
2024-01-17 18:57:32 +0530

    day 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,10 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {/* <AppRouterCacheProvider> */}
+      <body className={inter.className} suppressHydrationWarning={true}>
+        <AppRouterCacheProvider>
           {children}
-        {/* </AppRouterCacheProvider> */}
+        </AppRouterCacheProvider>
       </body>
     </html>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,164 @@
 import { useState } from 'react';
 import styles from './page.module.css';
 import Button from '@mui/material/Button';
-import { ButtonGroup, IconButton, Stack } from '@mui/material';
-import { FavoriteBorder, Favorite } from '@mui/icons-material';
+import { AppBar, Box, ButtonGroup, Container, IconButton, Stack, Toolbar, Typography, Menu, MenuItem, Tooltip, Avatar, TextField } from '@mui/material';
+import { FavoriteBorder, Favorite, Adb as AdbIcon, Menu as MenuIcon } from '@mui/icons-material';
+import javaScript from '../images/javascript.png'
+import Image from 'next/image';
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 export default function Home() {
   const [names, setNames] = useState('User1');
   const [isFavorite, setIsFavorite] = useState(false);
+
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
   return (
-    <main>
+
+    <>
+      <AppBar position="static" sx={{ bgcolor: '#2a2a2a', }}>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              LOGO
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+                disableScrollLock
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              LOGO
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt={names} src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+                disableScrollLock
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
       <div className={styles.main__container}>
         <h1>Hi! Welcome To New Next App {names} </h1>
+        <TextField id="outlined-basic" label="Name" variant="outlined" defaultValue={""} placeholder='Enter Your Name' type='text' autoFocus autoComplete={false} />
         <Stack direction={'row'} spacing={1}>
           <h1> Make Favorite  </h1>
           <IconButton size='large' color='primary' onClick={() => setIsFavorite(prev => !prev)} >
@@ -21,10 +169,11 @@ export default function Home() {
         </Stack>
 
         <ButtonGroup variant='outlined'>
-          <Button variant='contained' onClick={() => setNames('User2')}>Click Me</Button>
-          <Button variant="outlined" onClick={() => setNames("MUI")}>Change Name</Button>
+          <Button variant="outlined" onClick={() => setNames(document.getElementById("outlined-basic").value)}>Enter</Button>
         </ButtonGroup>
+        {/* <img src={javaScript}/> */}
+        <Image src={javaScript} alt='' />
       </div>
-    </main>
+    </>
   )
 }
