ad1de22171fa085d0294d1ca9a20bf132525f7d6
prasenjitDigitalFriend <100418932+prasenjitDigitalFriend@users.noreply.github.com>
2024-01-24 19:07:22 +0530

    day 5

---
+++ src/app/login/page.js
@@ -0,0 +1,73 @@
+'use client';
+import { Button, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, Stack, TextField } from '@mui/material'
+import React from 'react';
+import loginStyle from "./style.module.css"
+
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { useRouter } from 'next/navigation';
+
+const Login = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+  const router = useRouter();
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    router.replace("/home")
+  }
+
+
+  return (
+    <main className="main-container">
+      <div style={{
+        display: 'flex', flexDirection: 'column', alignItems: 'center',
+        justifyContent: 'center', minHeight: '100vh'
+      }}>
+        <div className={loginStyle.login_container}>
+          <div className={loginStyle.main_image}>
+            <img src='/images/login.jpg' alt="" />
+          </div>
+          <form className={loginStyle.login_form} onSubmit={(e)=>handleSubmit(e)}>
+            <TextField
+              label="Mobile"
+              id="outlined-start-adornment"
+              name='name'
+              sx={{ mb: 2, width: '100%' }}
+              InputProps={{
+                startAdornment: <InputAdornment position="start">+91</InputAdornment>,
+              }}
+            />
+            <FormControl sx={{ mb: 4, width: '100%' }} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+            <Button variant="contained" type="submit">Login</Button>
+          </form>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Login
--- src/app/page.js
+++ src/app/page.js
@@ -1,197 +1,32 @@
 'use client'
 
-import { useState } from 'react';
-import styles from './page.module.css';
-import Button from '@mui/material/Button';
-import { AppBar, Box, ButtonGroup, Container, IconButton, Stack, Toolbar, Typography, Menu, MenuItem, Tooltip, Avatar, TextField } from '@mui/material';
-import { FavoriteBorder, Favorite, Adb as AdbIcon, Menu as MenuIcon } from '@mui/icons-material';
-import Image from 'next/image';
-
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+import { Controls, Player } from "@lottiefiles/react-lottie-player"
+import { ArrowForward } from "@mui/icons-material"
+import { Button } from "@mui/material"
+import { useRouter } from "next/navigation"
 
 export default function Home() {
-  const [names, setNames] = useState('');
-  const [isFavorite, setIsFavorite] = useState(false);
 
-  const [anchorElNav, setAnchorElNav] = useState(null);
-  const [anchorElUser, setAnchorElUser] = useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
-  const AppBarComponent = () => {
-    return (
-      <AppBar position="static" sx={{ bgcolor: '#2a2a2a', }}>
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="#app-bar-with-responsive-menu"
-              sx={{
-                mr: 2,
-                display: { xs: 'none', md: 'flex' },
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'inherit',
-                textDecoration: 'none',
-              }}
-            >
-              LOGO
-            </Typography>
-
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'left',
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: 'block', md: 'none' },
-                }}
-                disableScrollLock
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-            <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="#app-bar-with-responsive-menu"
-              sx={{
-                mr: 2,
-                display: { xs: 'flex', md: 'none' },
-                flexGrow: 1,
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'inherit',
-                textDecoration: 'none',
-              }}
-            >
-              LOGO
-            </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-              {pages.map((page) => (
-                <Button
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                  {page}
-                </Button>
-              ))}
-            </Box>
-
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt={names} src="/static/images/avatar/2.jpg" />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: '45px' }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-                disableScrollLock
-              >
-                {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                    <Typography textAlign="center">{setting}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    )
-  }
+  const router = useRouter();
 
   return (
-    <>
-      <AppBarComponent />
-      <div className={styles.main__container}>
-        <Image alt='' src='/images/react.png' className={styles.react__img}
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: '20%', height: 'auto' }} />
-        <Image alt='' src='/images/javascript.png' className={styles.js__img}
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: '22%', height: 'auto' }} />
-        <h1 style={{ zIndex: 1, textTransform: 'capitalize' }} >Hi! <br /> Welcome {names} </h1>
-        {names == "" && <>
-          <form onSubmit={(e) => { e.preventDefault(); }} style={{ textAlign: 'center' }}>
-            <TextField id="outlined-basic" label="Name" variant="outlined" defaultValue={""} name='Name' placeholder='Enter Your Name' type='text' autoCorrect='off' autoComplete="off" sx={{
-              bgcolor: "AppWorkspace",
-              color: '#fff',
-              borderRadius: '10px',
-            }} InputLabelProps={{
-              sx: { color: '#fff', borderRadius: '10px', },
-            }} InputProps={{
-              sx: { color: '#fff', borderRadius: '10px', },
-            }} /> <br />
-            <Button variant="outlined" type='submit' sx={{ color: '#fff', borderRadius: '10px', mt: '10px' }} onClick={() => setNames(document.getElementById("outlined-basic").value)}>Enter</Button>
-          </form>
-        </>
-        }
+    <main className="main-container">
+      <div style={{
+        display: 'flex', flexDirection: 'column', alignItems: 'center',
+        justifyContent: 'center',marginTop: 'auto', marginBottom:'auto',height:'100%',paddingTop:'40px'
+      }}>
+        <Player
+          loop
+          autoplay
+          src={"/animation/main.json"}
+          style={{ maxHeight: '100%',height: '600px' }}
+        >
+        </Player>
+        <div style={{ textAlign: 'center' }}>
+          <Button variant="contained" endIcon={<ArrowForward />} onClick={() => router.replace("/login")} >Login / Signup</Button>
+        </div>
       </div>
-    </>
+    </main>
   )
 
 
